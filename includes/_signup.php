225d9ae089a336b7736a5b0a7be55400ef274d62
ashishshres <ashishshr27@gmail.com>
2024-05-23 15:04:32 +0545

    user signup handler

---
+++ includes/_signup.php
@@ -0,0 +1,25 @@
+<?php
+include "_dbconnect.php";
+
+$showAlert = false;
+$exists = false;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = htmlspecialchars($_POST["username"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $password = htmlspecialchars($_POST["password"]);
+
+    // check for the duplicate entries
+    $sql = "SELECT * FROM users WHERE username = '$username' AND email = '$email'";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        $exists = true;
+    } else {
+        $hash = password_hash($password, PASSWORD_DEFAULT);
+        $sql = "INSERT INTO users (username, email, password) VALUES('$username','$email', '$hash')";
+        $result = mysqli_query($conn, $sql);
+        if ($result) {
+            $showAlert = true;
+        }
+    }
+}
