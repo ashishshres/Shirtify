1656887fc0f064fa4b68050513466c2485aa6bf3
ashishshres <ashishshr27@gmail.com>
2024-05-23 15:04:25 +0545

    user signin handler

---
+++ includes/_signin.php
@@ -0,0 +1,27 @@
+<?php
+include "_dbconnect.php";
+
+$showError = false;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = htmlspecialchars($_POST["email"]);
+    $password = htmlspecialchars($_POST["password"]);
+
+    // check for the email and password
+    $sql = "SELECT * FROM users WHERE email = '$email'";
+    $result = mysqli_query($conn, $sql);
+    $isFound = mysqli_num_rows($result);
+    if ($isFound == 1) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            if (password_verify($password, $row["password"])) {
+                session_start();
+                $_SESSION["loggedin"] = true;
+                $_SESSION["username"] = $row["username"];
+                header("location: ../index.php");
+            } else {
+                $showError = true;
+            }
+        }
+    } else {
+        $showError = true;
+    }
+}
