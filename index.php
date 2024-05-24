06ee603e44c7bab974e1fede8477ccd41105f31c
ashishshres <ashishshr27@gmail.com>
2024-05-24 20:51:41 +0545

    refactor index.php

--- index.php
+++ index.php
@@ -49,6 +49,25 @@ include "./includes/_add_cart.php";
         </div>
         ';
     }
+
+    if (isset($_GET['exists'])) {
+        echo ' 
+    <div class="transition duration-300 ease-in-out toast-signup fixed top-20 right-5 mx-2 max-w-xs bg-white border border-gray-200 rounded-xl shadow-lg" role="alert">
+        <div class="flex p-4">
+            <div class="flex-shrink-0">
+                <svg class="flex-shrink-0 size-4 text-teal-500 mt-0.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"></path>
+                </svg>
+            </div>
+            <div class="ms-3">
+                <p class="text-sm text-gray-900">
+                    Product is already added to cart.
+                </p>
+            </div>
+        </div>
+    </div>
+    ';
+    }
     ?>
 
     <div>
