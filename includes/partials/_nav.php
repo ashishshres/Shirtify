14f51c1ce4fab2615d11d6e98be3234c2eaad3db
ashishshres <ashishshr27@gmail.com>
2024-05-24 19:56:43 +0545

    add partials

---
+++ includes/partials/_nav.php
@@ -0,0 +1,8 @@
+<div class="bg-gray-100 shadow-sm px-8 py-3 flex flex-wrap justify-between items-center">
+    <div class="text-blue-700 text-3xl font-bold cursor-pointer" onclick="window.location.href='../index.php'">Shirtify.</div>
+    <div class="flex flex-wrap gap-4 items-center w-full md:w-max">
+        <a href="./cart.php" class="font-medium hover:underline">Cart</a>
+        <a href="./account.php" class="font-medium hover:underline">My Account</a>
+        <button type="submit" name="logout" onclick="window.location.href='../includes/_logout.php'" class="bg-zinc-800 text-white px-4 py-2 rounded-md hover:bg-zinc-700 w-full md:w-max">Logout</button>
+    </div>
+</div>
