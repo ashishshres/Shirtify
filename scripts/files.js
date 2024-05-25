ef2ce049aeae39e6767c6ac44ce2931ebb747a9f
ashishshres <ashishshr27@gmail.com>
2024-05-25 13:41:33 +0545

    handle files

---
+++ scripts/files.js
@@ -0,0 +1,11 @@
+function updateFileName() {
+    const input = document.getElementById("productimage");
+    const fileName = input.files[0] ? input.files[0].name : "Not Chosen";
+    document.getElementById("file-chosen").textContent = fileName;
+}
+
+function updateAvatar() {
+    const input = document.getElementById("avatar");
+    const fileName = input.files[0] ? input.files[0].name : "Not Chosen";
+    document.getElementById("file-chosen").textContent = fileName;
+}
