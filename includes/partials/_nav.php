66aadae394ee2368366a51af3a864a45f1102bf9
ashishshres <ashishshr27@gmail.com>
2024-05-25 16:57:51 +0545

    add avatar

--- includes/partials/_nav.php
+++ includes/partials/_nav.php
@@ -2,7 +2,7 @@
     <div class="text-blue-700 text-3xl font-bold cursor-pointer" onclick="window.location.href='../index.php'">Shirtify.</div>
     <div class="flex flex-wrap gap-4 items-center w-full md:w-max">
         <a href="./cart.php" class="font-medium hover:underline">Cart</a>
-        <a href="./account.php" class="font-medium hover:underline">My Account</a>
+        <a href="./account.php" class="font-medium hover:underline flex items-center gap-2">My Account <span class="inline-block h-8 w-8"><img class="h-full w-full object-cover rounded-full" src="<?= $_SESSION["avatar"] ?>" alt=""></span></a>
         <button type="submit" name="logout" onclick="window.location.href='../includes/_logout.php'" class="bg-zinc-800 text-white px-4 py-2 rounded-md hover:bg-zinc-700 w-full md:w-max">Logout</button>
     </div>
 </div>
