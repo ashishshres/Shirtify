d35122851a38b7692b886de5ab9bfe52852b65bb
ashishshres <ashishshr27@gmail.com>
2024-05-25 17:06:01 +0545

    add favicon

--- pages/signin.php
+++ pages/signin.php
@@ -10,6 +10,7 @@ include "../includes/_signin.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign in - Shirtify</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="shortcut icon" href="../images/favicon_io/favicon.ico" type="image/x-icon">
 </head>
 
 <body class="h-screen w-screen">
