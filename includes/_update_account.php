ac7695a0fb9262efff4823474b45d1d9b2e14210
ashishshres <ashishshr27@gmail.com>
2024-05-25 13:41:12 +0545

    add _update_account.php

---
+++ includes/_update_account.php
@@ -0,0 +1,45 @@
+<?php
+include "partials/_dbconnect.php";
+$showAlert = false;
+$currentUsername = $_SESSION["username"];
+
+// sql query to select table row
+$sql = "SELECT * FROM users WHERE username = '$currentUsername'";
+$result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_assoc($result);
+$avatarPath = $row["avatar"];
+
+if (isset($_POST["update"])) {
+    $username = $_POST["username"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    if (isset($_FILES["avatar"])) {
+        $filename = $_FILES["avatar"]["name"];
+        $filesize = $_FILES["avatar"]["size"];
+        $filetmp = $_FILES["avatar"]["tmp_name"];
+        $filetype = $_FILES["avatar"]["type"];
+        $avatarPath = "../includes/avatar/$filename";
+        move_uploaded_file($filetmp, "../includes/avatar/" . $filename);
+    }
+
+    if (empty($password)) {
+        // no password update
+        $password = $row["password"];
+        $sql = "UPDATE users SET username = '$username', email = '$email', avatar = '$avatarPath' WHERE username = '$currentUsername'";
+    } else {
+        // password update
+        $hash = password_hash($password, PASSWORD_DEFAULT);
+        $sql = "UPDATE users SET username = '$username', email = '$email', password = '$hash', avatar = '$avatarPath' WHERE username = '$currentUsername'";
+    }
+
+    if (mysqli_query($conn, $sql)) {
+        $showAlert = true;
+        // Update session username
+        $_SESSION["username"] = $username;
+        $_SESSION["email"] = $email;
+        $_SESSION["avatar"] = $avatarPath;
+        header("Location: ./account.php?success=1");
+        exit();
+    }
+}
