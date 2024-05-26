9a42d52ac45cfe3febda767ee11a408a14d30ba4
ashishshres <ashishshr27@gmail.com>
2024-05-26 21:56:50 +0545

    add show password

--- pages/signup.php
+++ pages/signup.php
@@ -77,6 +77,10 @@ include "../includes/_signup.php";
                             <label for="password">Password</label>
                             <input type="password" name="password" id="password" class="bg-zinc-200 p-2 rounded-md">
                         </div>
+                        <div class="flex gap-1">
+                            <input type="checkbox" name="show" id="show" class="bg-zinc-200 p-2 rounded-md" onclick="togglePassword()">
+                            <label for="show">Show Password</label>
+                        </div>
                         <div class="flex flex-col gap-1 text-center">
                             <button type="submit" name="signup" class="bg-zinc-800 text-white p-2 rounded-md hover:bg-zinc-700">Sign Up</button>
                             <p>Already have an account? <span class="font-medium hover:underline cursor-pointer" onclick="window.location.href='./signin.php'">Sign in</span></p>
