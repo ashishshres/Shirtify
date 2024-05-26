e889dfbe58cf5b159e0c55aa50d8f8da4c6e52d0
ashishshres <ashishshr27@gmail.com>
2024-05-26 21:57:29 +0545

    add toggle password function

--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,54 @@ if (toastSignup) {
         toastSignup.style.opacity = 0;
     }, 5000);
 }
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     const modal = document.querySelector("#hs-basic-modal");
+//     const deleteButtons = document.querySelectorAll(".delete");
+//     let productIdToDelete;
+
+//     if (modal && deleteButtons) {
+//         deleteButtons.forEach((button) => {
+//             button.addEventListener("click", () => {
+//                 productIdToDelete = button.getAttribute("data-id");
+//                 modal.style.opacity = 1;
+//                 modal.style.display = "block";
+//             });
+//         });
+
+//         // close modal
+//         const closeModalButtons =
+//             document.querySelectorAll("[data-hs-overlay]");
+//         closeModalButtons.forEach((button) => {
+//             button.addEventListener("click", () => {
+//                 modal.style.opacity = 0;
+//                 modal.style.pointerEvents = "none";
+//                 modal.style.display = "none";
+//             });
+//         });
+
+//         // handle delete confirmation
+//         const confirmDeleteButton = modal.querySelector(".confirm-delete");
+//         confirmDeleteButton.addEventListener("click", () => {
+//             if (productIdToDelete) {
+//                 window.location.href = `./product.php?deleteid=${productIdToDelete}`;
+//             }
+//         });
+//     }
+// });
+
+function togglePassword() {
+    let password = document.querySelector("#password");
+    let showPassword = document.querySelector("#show");
+
+    // toggle password visibility
+    if (showPassword) {
+        showPassword.addEventListener("change", function () {
+            if (this.checked) {
+                password.setAttribute("type", "text");
+            } else {
+                password.setAttribute("type", "password");
+            }
+        });
+    }
+}
