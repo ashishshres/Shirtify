616c7a4ab0efc01359310f00ddd5e6760c17fa2c
ashishshres <ashishshr27@gmail.com>
2024-05-27 22:27:27 +0545

    refactor signup.php

--- pages/signup.php
+++ pages/signup.php
@@ -1,4 +1,12 @@
 <?php
+session_start();
+
+// Check if the user is already logged in
+if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
+    header("location: ../index.php");
+    exit;
+}
+
 include "../includes/_signup.php";
 ?>
 
