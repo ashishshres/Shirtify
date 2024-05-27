2c447d18d19b74d9a31b607545917b771e2f2a32
ashishshres <ashishshr27@gmail.com>
2024-05-27 22:25:59 +0545

    add quantity

--- includes/_display_cart.php
+++ includes/_display_cart.php
@@ -4,7 +4,6 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] != true) {
     header("location: ./signin.php");
     exit;
 }
-
 include "partials/_dbconnect.php";
 
 $sn = 0;
@@ -21,23 +20,28 @@ if (mysqli_num_rows($result) > 0) {
         $productimage = $row["image_path"];
         $productname = $row["product_name"];
         $productprice = $row["product_price"];
+        $quantity = $row["quantity"];
+
         echo '<div class="rounded-lg border border-gray-200 bg-white p-4 shadow-sm md:p-6">
         <div class="space-y-4 md:flex md:items-center md:justify-between md:gap-6 md:space-y-0">
                 <img class="h-20 w-20 object-cover" src="' . $productimage . '" alt="imac image" />
             <label for="counter-input" class="sr-only">Choose quantity:</label>
             <div class="flex items-center justify-between md:order-3 md:justify-end">
                 <div class="flex items-center">
-                    <button type="button" id="decrement-button-4" data-input-counter-decrement="counter-input-4" class="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100 ">
-                        <svg class="h-2.5 w-2.5 text-gray-900 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h16" />
-                        </svg>
-                    </button>
-                    <input type="text" id="counter-input-4" data-input-counter class="w-10 shrink-0 border-0 bg-transparent text-center text-sm font-medium text-gray-900 focus:outline-none focus:ring-0 " placeholder="" value="1" required />
-                    <button type="button" id="increment-button-4" data-input-counter-increment="counter-input-4" class="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100 ">
-                        <svg class="h-2.5 w-2.5 text-gray-900 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
-                        </svg>
-                    </button>
+                    <form action="../includes/_update_cart.php" method = "post">
+                        <input type = "hidden" name="update" value = "' . $productid . '">
+                        <button type="submit" name="decrement" id="decrement-button-4" data-input-counter-decrement="counter-input-4" class="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100 ">
+                            <svg class="h-2.5 w-2.5 text-gray-900 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h16" />
+                            </svg>
+                        </button>
+                        <input type="text" name="update-quantity" id="counter-input-4" data-input-counter class="w-10 shrink-0 border-0 bg-transparent text-center text-sm font-medium text-gray-900 focus:outline-none focus:ring-0 " placeholder="" value="' . $quantity . '"/>
+                        <button type="submit" name="increment" id="increment-button-4" data-input-counter-increment="counter-input-4" class="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100 ">
+                            <svg class="h-2.5 w-2.5 text-gray-900 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
+                            </svg>
+                        </button>
+                    </form>
                 </div>
                 <div class="text-end md:order-4 md:w-32">
                     <p class="text-base font-bold text-gray-900 ">Rs.' . $productprice . '</p>
