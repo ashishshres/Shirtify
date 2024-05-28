9e35ecb082533a8f03f0127356bd008b8feeefbd
ashishshres <ashishshr27@gmail.com>
2024-05-28 09:23:49 +0545

    add product.php

---
+++ pages/product.php
@@ -0,0 +1,86 @@
+<?php
+session_start();
+
+// check if the user is loggedin
+if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] != true) {
+    header("location: ./signin.php");
+    exit;
+}
+
+include "../includes/_delete_product.php";
+?>
+
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Products - Shirtify</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="shortcut icon" href="../images/favicon/favicon.ico" type="image/x-icon">
+</head>
+
+<body>
+    <?php include "../includes/partials/_nav.php" ?>
+    <div id="hs-basic-modal" class="hs-overlay hs-overlay-open:opacity-100 hs-overlay-open:duration-500  size-full fixed top-0 start-0 z-[80] opacity-0 overflow-x-hidden transition-all overflow-y-auto pointer-events-none hidden">
+        <div class="shadow-lg sm:max-w-lg sm:w-full m-3 sm:mx-auto">
+            <div class="flex flex-col bg-white border shadow-sm rounded-xl pointer-events-auto">
+                <div class="flex justify-between items-center py-3 px-4 border-b ">
+                    <h3 class="font-bold text-gray-800 ">
+                        Alert
+                    </h3>
+                    <button type="button" class="flex justify-center items-center size-7 text-sm font-semibold rounded-full border border-transparent text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none " data-hs-overlay="#hs-basic-modal">
+                        <span class="sr-only">Close</span>
+                        <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                            <path d="M18 6 6 18"></path>
+                            <path d="m6 6 12 12"></path>
+                        </svg>
+                    </button>
+                </div>
+                <div class="p-4 overflow-y-auto">
+                    <p class="mt-1 text-gray-800 ">
+                        Confirm you want to delete the product.
+                    </p>
+                </div>
+                <div class="flex justify-end items-center gap-x-2 py-3 px-4 border-t ">
+                    <button type="button" class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none " data-hs-overlay="#hs-basic-modal">
+                        Cancel
+                    </button>
+                    <button type="button" class="confirm-delete py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-red-600 text-white hover:bg-red-700 disabled:opacity-50 disabled:pointer-events-none">
+                        Delete
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="flex flex-wrap gap-8 px-8 py-5">
+        <a href="./account.php" class="text-xl">My Account</a>
+        <a href="./product.php" class="text-xl font-bold">My Products</a>
+        <a href="./create_product.php" class="text-xl">Create New Product</a>
+    </div>
+    <div class="flex flex-col px-8">
+        <div class="-m-1.5 overflow-x-auto">
+            <div class="p-1.5 min-w-full inline-block align-middle">
+                <div class="overflow-hidden">
+                    <table class="min-w-full divide-y divide-gray-200 ">
+                        <thead>
+                            <tr>
+                                <th scope="col" class="px-4 py-3 text-start font-bold text-gray-800 uppercase">S.N.</th>
+                                <th scope="col" class="px-4 py-3 text-start font-bold text-gray-800 uppercase">Product Name</th>
+                                <th scope="col" class="px-4 py-3 text-start font-bold text-gray-800 uppercase">Product Price</th>
+                                <th scope="col" class="px-4 py-3 text-start font-bold text-gray-800 uppercase">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php include "../includes/_added_product.php" ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="../scripts/main.js"></script>
+</body>
+
+</html>
