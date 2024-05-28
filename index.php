a393b017c83ec61d1104dbb54dca33df14420584
ashishshres <ashishshr27@gmail.com>
2024-05-28 09:05:11 +0545

    refactor: add favicon

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ include "./includes/_cart_number.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shop - Shirtify</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="shortcut icon" href="./images/favicon_io/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="./images/favicon/favicon.ico" type="image/x-icon">
 </head>
 
 <body>
