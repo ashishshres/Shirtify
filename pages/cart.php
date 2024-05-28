2616f7d9ba05575ebeaf580f2e1d7bb4e1badbb0
ashishshres <ashishshr27@gmail.com>
2024-05-28 09:23:32 +0545

    refactor: calculate total amount

--- pages/cart.php
+++ pages/cart.php
@@ -52,43 +52,11 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] != true) {
             <div class="mt-3 md:gap-6 lg:flex lg:items-start xl:gap-8">
                 <div class="w-full flex-none lg:max-w-2xl xl:max-w-4xl">
                     <div class="space-y-6 mb-4">
-                        <?php include "../includes/_display_cart.php";
-                        ?>
+                        <?php include "../includes/_display_cart.php" ?>
                     </div>
                 </div>
                 <div class="mx-auto mt-6 max-w-4xl flex-1 space-y-6 lg:mt-0 lg:w-full">
-                    <div class="space-y-4 rounded-lg border border-gray-200 bg-white p-4 shadow-sm  sm:p-6">
-                        <p class="text-xl font-semibold text-gray-900 ">Order summary</p>
-                        <div class="space-y-4">
-                            <div class="space-y-2">
-                                <dl class="flex items-center justify-between gap-4">
-                                    <dt class="text-base font-normal text-gray-500 ">Total MRP</dt>
-                                    <dd class="text-base font-medium text-gray-900 ">Rs.0.00</dd>
-                                </dl>
-
-                                <dl class="flex items-center justify-between gap-4">
-                                    <dt class="text-base font-normal text-gray-500 ">Delivery Fee</dt>
-                                    <dd class="text-base font-medium text-green-600">Rs.0.00</dd>
-                                </dl>
-                            </div>
-
-                            <dl class="flex items-center justify-between gap-4 border-t border-gray-200 pt-2 ">
-                                <dt class="text-base font-bold text-gray-900 ">Total Amount</dt>
-                                <dd class="text-base font-bold text-gray-900 ">Rs.0.00</dd>
-                            </dl>
-                        </div>
-
-                        <a href="#" class="flex w-full items-center justify-center rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium text-white bg-zinc-900 hover:bg-zinc-700 focus:outline-none focus:ring-4 focus:ring-primary-300 ">Proceed to Checkout</a>
-                        <div class="flex items-center justify-center gap-2">
-                            <span class="text-sm font-normal text-gray-500 "> or </span>
-                            <a href="#" title="" class="inline-flex items-center gap-2 text-sm font-medium text-primary-700 underline hover:no-underline ">
-                                Continue Shopping
-                                <svg class="h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 12H5m14 0-4 4m4-4-4-4" />
-                                </svg>
-                            </a>
-                        </div>
-                    </div>
+                    <?php include "../includes/_display_total.php" ?>
                 </div>
             </div>
         </div>
