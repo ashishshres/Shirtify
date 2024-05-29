4af3c7181b4008caf351388bc2dc45955e9a5dd7
ashishshres <ashishshr27@gmail.com>
2024-05-29 08:05:43 +0545

    refactor main.js

--- scripts/main.js
+++ scripts/main.js
@@ -5,40 +5,40 @@ if (toastSignup) {
     }, 5000);
 }
 
-// document.addEventListener("DOMContentLoaded", function () {
-//     const modal = document.querySelector("#hs-basic-modal");
-//     const deleteButtons = document.querySelectorAll(".delete");
-//     let productIdToDelete;
+document.addEventListener("DOMContentLoaded", function () {
+    const modal = document.querySelector("#hs-basic-modal");
+    const deleteButtons = document.querySelectorAll(".delete");
+    let productIdToDelete;
 
-//     if (modal && deleteButtons) {
-//         deleteButtons.forEach((button) => {
-//             button.addEventListener("click", () => {
-//                 productIdToDelete = button.getAttribute("data-id");
-//                 modal.style.opacity = 1;
-//                 modal.style.display = "block";
-//             });
-//         });
+    if (modal && deleteButtons) {
+        deleteButtons.forEach((button) => {
+            button.addEventListener("click", () => {
+                productIdToDelete = button.getAttribute("data-id");
+                modal.style.opacity = 1;
+                modal.style.display = "block";
+            });
+        });
 
-//         // close modal
-//         const closeModalButtons =
-//             document.querySelectorAll("[data-hs-overlay]");
-//         closeModalButtons.forEach((button) => {
-//             button.addEventListener("click", () => {
-//                 modal.style.opacity = 0;
-//                 modal.style.pointerEvents = "none";
-//                 modal.style.display = "none";
-//             });
-//         });
+        // close modal
+        const closeModalButtons =
+            document.querySelectorAll("[data-hs-overlay]");
+        closeModalButtons.forEach((button) => {
+            button.addEventListener("click", () => {
+                modal.style.opacity = 0;
+                modal.style.pointerEvents = "none";
+                modal.style.display = "none";
+            });
+        });
 
-//         // handle delete confirmation
-//         const confirmDeleteButton = modal.querySelector(".confirm-delete");
-//         confirmDeleteButton.addEventListener("click", () => {
-//             if (productIdToDelete) {
-//                 window.location.href = `./product.php?deleteid=${productIdToDelete}`;
-//             }
-//         });
-//     }
-// });
+        // handle delete confirmation
+        const confirmDeleteButton = modal.querySelector(".confirm-delete");
+        confirmDeleteButton.addEventListener("click", () => {
+            if (productIdToDelete) {
+                window.location.href = `./product.php?deleteid=${productIdToDelete}`;
+            }
+        });
+    }
+});
 
 function togglePassword() {
     let password = document.querySelector("#password");
