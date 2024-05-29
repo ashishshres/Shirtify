125ef16212dd206fddf5e0a589b8088abbb8a6a2
ashishshres <ashishshr27@gmail.com>
2024-05-29 08:06:09 +0545

    add file uploads

--- includes/_update_product.php
+++ includes/_update_product.php
@@ -15,15 +15,30 @@ if (isset($_GET["updateid"])) {
     $row = mysqli_fetch_assoc($result);
 
     if (isset($_POST["update"])) {
+        $currentuser = $_SESSION["username"];
         $productname = $_POST["productname"];
         $productprice = $_POST["productprice"];
+        $currentname = $_POST["currentname"];
+        $currentprice = $_POST["currentprice"];
+        $imagePath = $_POST["currentimage"];
+
+        if (isset($_FILES["product-image"]) && $_FILES["product-image"]["error"] == 0) {
+            $filename = $_FILES["product-image"]["name"];
+            $filesize = $_FILES["product-image"]["size"];
+            $filetmp = $_FILES["product-image"]["tmp_name"];
+            $filetype = $_FILES["product-image"]["type"];
+            $cartImagePath = "../uploads/$filename";
+            $productImagePath = "./uploads/$filename";
+            move_uploaded_file($filetmp, "../uploads/" . $filename);
+        }
+
+        $sql = "UPDATE cart SET product_name = '$productname', product_price = '$productprice', image_path = '$cartImagePath' WHERE product_name = '$currentname' AND added_by = '$currentuser'";
+        mysqli_query($conn, $sql);
 
         // sql query to update  table
-        $sql = "UPDATE products SET product_name = '$productname', product_price = '$productprice' WHERE id = $id";
+        $sql = "UPDATE products SET product_name = '$productname', product_price = '$productprice', image_path = '$productImagePath' WHERE id = $id";
+        mysqli_query($conn, $sql);
 
-        if (mysqli_query($conn, $sql)) {
-            $showAlert = true;
-            header("location: ./update_product.php?updateid=$id&success=1");
-        }
+        header("location: ./update_product.php?updateid=$id&success=1");
     }
 }
