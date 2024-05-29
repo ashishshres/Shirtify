e724be07a8baa2ec0e25ecba503cc59ad0745637
ashishshres <ashishshr27@gmail.com>
2024-05-29 21:12:13 +0545

    refactor: add delete from cart

--- includes/_delete_product.php
+++ includes/_delete_product.php
@@ -4,7 +4,16 @@ $showAlert = false;
 
 if (isset($_GET["deleteid"])) {
     $id = $_GET["deleteid"];
-    // sql query to select table row
+
+    $productsql = "SELECT * FROM products where id = $id";
+    $result = mysqli_query($conn, $productsql);
+    $row = mysqli_fetch_assoc($result);
+    $currentProduct = $row["product_name"];
+    $currentuser = $_SESSION["username"];
+
+    $sql = "DELETE FROM cart WHERE product_name = '$currentProduct' AND added_by = '$currentuser'";
+    mysqli_query($conn, $sql);
+
     $sql = "DELETE FROM products WHERE id = $id";
 
     if (mysqli_query($conn, $sql)) {
