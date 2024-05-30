b5505da70d2b9cf4fcd7962f5259fe4cfaf8e657
ashishshres <ashishshr27@gmail.com>
2024-05-30 17:35:05 +0545

    fix: update product image path

--- includes/_update_product.php
+++ includes/_update_product.php
@@ -4,6 +4,7 @@ $showAlert = false;
 
 if (isset($_GET["updateid"])) {
     $id = $_GET["updateid"];
+
     // sql query to select table row
     $sql = "SELECT * FROM products WHERE id = $id";
     $result = mysqli_query($conn, $sql);
@@ -20,9 +21,8 @@ if (isset($_GET["updateid"])) {
         $productprice = $_POST["productprice"];
         $currentname = $_POST["currentname"];
         $currentprice = $_POST["currentprice"];
-        $imagePath = $_POST["currentimage"];
 
-        if (isset($_FILES["product-image"]) && $_FILES["product-image"]["error"] == 0) {
+        if (isset($_FILES["product-image"]) && $_FILES["product-image"]["size"] > 0) {
             $filename = $_FILES["product-image"]["name"];
             $filesize = $_FILES["product-image"]["size"];
             $filetmp = $_FILES["product-image"]["tmp_name"];
@@ -30,12 +30,15 @@ if (isset($_GET["updateid"])) {
             $cartImagePath = "../uploads/$filename";
             $productImagePath = "./uploads/$filename";
             move_uploaded_file($filetmp, "../uploads/" . $filename);
+        } else {
+            $cartImagePath = '.' . $row["image_path"];
+            $productImagePath = $row["image_path"];
         }
 
         $sql = "UPDATE cart SET product_name = '$productname', product_price = '$productprice', image_path = '$cartImagePath' WHERE product_name = '$currentname' AND added_by = '$currentuser'";
         mysqli_query($conn, $sql);
 
-        // sql query to update  table
+        // sql query to update table
         $sql = "UPDATE products SET product_name = '$productname', product_price = '$productprice', image_path = '$productImagePath' WHERE id = $id";
         mysqli_query($conn, $sql);
 
